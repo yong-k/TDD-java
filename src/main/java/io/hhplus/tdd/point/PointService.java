db1d5522ad4a19de03c9c0d0b55a7491ff18990b
jeongyong <cin03023@gmail.com>
2025-05-23 04:09:58 +0900

    fix: PointService - 동시성 처리 추가 (ConcurrentHashMap, synchronized)

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +18,13 @@ public class PointService {
     private static final int MAX_CHARGE = 2_000_000;    // 1회 최대 충전 가능 포인트
     private static final int MAX_POINT = 2_000_000;     // 최대 보유 가능 포인트
 
+    // userId 별로 Lock객체(빈 Object 인스턴스) 만들어서 관리
+    private final ConcurrentHashMap<Long, Object> userLock = new ConcurrentHashMap<>();
+
+    private Object getLock(long userId) {
+        return userLock.computeIfAbsent(userId, id -> new Object());
+    }
+
     public UserPoint selectById(long id) {
         return userPointTable.selectById(id);
     }
@@ -26,33 +34,40 @@ public class PointService {
     }
 
     public UserPoint charge(long userId, long amount) {
-        // 1회 최대 충전 금액 체크
-        if (amount > MAX_CHARGE)
-            throw new IllegalArgumentException("1회 최대 충전 금액은 " + String.format("%,d", MAX_CHARGE) + "원입니다.");
+        // 동시성 처리
+        synchronized (getLock(userId)) {
 
-        UserPoint beforeCharge = userPointTable.selectById(userId);
+            // 1회 최대 충전 금액 체크
+            if (amount > MAX_CHARGE)
+                throw new IllegalArgumentException("1회 최대 충전 금액은 " + String.format("%,d", MAX_CHARGE) + "원입니다.");
 
-        // 최대 보유 포인트 초과여부 체크
-        long afterChargePoint = beforeCharge.point() + amount;
-        if (afterChargePoint > MAX_POINT)
-            throw new IllegalArgumentException("포인트는 최대 " + String.format("%,d", MAX_POINT) + "원까지 보유할 수 있습니다.");
+            UserPoint beforeCharge = userPointTable.selectById(userId);
 
-        UserPoint afterCharge = userPointTable.insertOrUpdate(userId, afterChargePoint);
-        pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+            // 최대 보유 포인트 초과여부 체크
+            long afterChargePoint = beforeCharge.point() + amount;
+            if (afterChargePoint > MAX_POINT)
+                throw new IllegalArgumentException("포인트는 최대 " + String.format("%,d", MAX_POINT) + "원까지 보유할 수 있습니다.");
 
-        return afterCharge;
+            UserPoint afterCharge = userPointTable.insertOrUpdate(userId, afterChargePoint);
+            pointHistoryTable.insert(userId, amount, TransactionType.CHARGE, System.currentTimeMillis());
+
+            return afterCharge;
+        }
     }
 
     public UserPoint use(long userId, long amount) {
-        UserPoint beforeUse = userPointTable.selectById(userId);
+        // 동시성 처리
+        synchronized (getLock(userId)) {
+            UserPoint beforeUse = userPointTable.selectById(userId);
 
-        // 보유포인트 < 사용포인트 체크
-        if (beforeUse.point() < amount)
-            throw new IllegalArgumentException("포인트가 부족합니다.");
+            // 보유포인트 < 사용포인트 체크
+            if (beforeUse.point() < amount)
+                throw new IllegalArgumentException("포인트가 부족합니다.");
 
-        UserPoint afterUse = userPointTable.insertOrUpdate(userId, beforeUse.point() - amount);
-        pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
+            UserPoint afterUse = userPointTable.insertOrUpdate(userId, beforeUse.point() - amount);
+            pointHistoryTable.insert(userId, amount, TransactionType.USE, System.currentTimeMillis());
 
-        return afterUse;
+            return afterUse;
+        }
     }
 }
