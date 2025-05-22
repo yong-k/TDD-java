15f58f91044c80fdc7edb4946b010054fc9e50ee
jeongyong <cin03023@gmail.com>
2025-05-23 04:05:17 +0900

    feat: PointControllerTest - 포인트사용 (Green)

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -44,10 +44,7 @@ public class PointController {
      * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
      */
     @PatchMapping("{id}/use")
-    public UserPoint use(
-            @PathVariable long id,
-            @RequestBody long amount
-    ) {
-        return new UserPoint(0, 0, 0);
+    public UserPoint use(@PathVariable long id, @RequestBody long amount) {
+        return pointService.use(id, amount);
     }
 }
