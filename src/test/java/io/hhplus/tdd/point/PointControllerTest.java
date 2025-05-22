04fd6c6397f4f51130c36dba6d084e7404ad2424
jeongyong <cin03023@gmail.com>
2025-05-23 04:03:51 +0900

    test: PointControllerTest - 포인트충전 (Red)

--- src/test/java/io/hhplus/tdd/point/PointControllerTest.java
+++ src/test/java/io/hhplus/tdd/point/PointControllerTest.java
@@ -4,14 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(PointController.class)
 class PointControllerTest {
@@ -56,5 +57,24 @@ class PointControllerTest {
                 .andExpect(jsonPath("$[0].type").value("CHARGE"))
                 .andExpect(jsonPath("$[1].amount").value(5000))
                 .andExpect(jsonPath("$[1].type").value("USE"));
+    }
+
+    @Test
+    void 포인트충전() throws Exception {
+        // given
+        long userId = 1L;
+        long amount = 5000;
+        UserPoint expected = new UserPoint(userId, 15000, System.currentTimeMillis());
+        when(pointService.charge(userId, amount)).thenReturn(expected);
+
+        // when
+        // then
+        mockMvc.perform(patch("/point/{id}/charge", userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"amount\":5000}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(expected.id()))
+                .andExpect(jsonPath("$.point").value(expected.point()))
+                .andDo(print());
     }
 }
