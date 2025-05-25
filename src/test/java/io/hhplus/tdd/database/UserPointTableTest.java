39235e54416ee4863f446da5f76d4ee5d6cf1b90
jeongyong <cin03023@gmail.com>
2025-05-25 16:23:32 +0900

    test: UserPointTableTest - insertOrUpdate_기존, 신규

---
+++ src/test/java/io/hhplus/tdd/database/UserPointTableTest.java
@@ -0,0 +1,50 @@
+package io.hhplus.tdd.database;
+
+import io.hhplus.tdd.point.UserPoint;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.*;
+
+class UserPointTableTest {
+
+    private UserPointTable userPointTable;
+
+    @BeforeEach
+    void setUp() {
+        userPointTable = new UserPointTable();
+    }
+
+    @Test
+    void insertOrUpdate_신규회원() {
+        // given
+        long userId = 1L;
+        long amount = 10000;
+
+        // when
+        UserPoint actual = userPointTable.insertOrUpdate(userId, amount);
+
+        // then
+        assertThat(actual).isNotNull();
+        assertThat(actual.id()).isEqualTo(userId);
+        assertThat(actual.point()).isEqualTo(amount);
+        assertThat(userPointTable.selectById(userId)).isEqualTo(actual);
+    }
+
+    @Test
+    void insertOrUpdate_기존회원() {
+        // given
+        long userId = 1L;
+
+        // when
+        UserPoint insert = userPointTable.insertOrUpdate(userId, 10000);
+        UserPoint update = userPointTable.insertOrUpdate(userId, 5000);
+
+        // then
+        assertThat(update).isNotNull();
+        assertThat(update.id()).isEqualTo(userId);
+        assertThat(update.point()).isEqualTo(15000);
+        assertThat(userPointTable.selectById(userId)).isEqualTo(update);
+    }
+}
