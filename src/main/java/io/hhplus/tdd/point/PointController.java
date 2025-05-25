81d6281685636aba48999064e596f0273d9b2808
jeongyong <cin03023@gmail.com>
2025-05-25 17:35:40 +0900

    refactor: ResponseEntity 사용

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -3,6 +3,7 @@ package io.hhplus.tdd.point;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,35 +17,27 @@ public class PointController {
 
     private final PointService pointService;
 
-    /**
-     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
-     */
     @GetMapping("{id}")
-    public UserPoint point(@PathVariable long id) {
-        return pointService.selectById(id);
+    public ResponseEntity<UserPoint> point(@PathVariable long id) {
+        UserPoint userPoint = pointService.selectById(id);
+        return ResponseEntity.ok(userPoint);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
-     */
     @GetMapping("{id}/histories")
-    public List<PointHistory> history(@PathVariable long id) {
-        return pointService.selectHistoryById(id);
+    public ResponseEntity<List<PointHistory>> history(@PathVariable long id) {
+        List<PointHistory> histories = pointService.selectHistoryById(id);
+        return ResponseEntity.ok(histories);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
-     */
     @PatchMapping("{id}/charge")
-    public UserPoint charge(@PathVariable long id, @RequestBody long amount) {
-        return pointService.charge(id, amount);
+    public ResponseEntity<UserPoint> charge(@PathVariable long id, @RequestBody long amount) {
+        UserPoint charge = pointService.charge(id, amount);
+        return ResponseEntity.ok(charge);
     }
 
-    /**
-     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
-     */
     @PatchMapping("{id}/use")
-    public UserPoint use(@PathVariable long id, @RequestBody long amount) {
-        return pointService.use(id, amount);
+    public ResponseEntity<UserPoint> use(@PathVariable long id, @RequestBody long amount) {
+        UserPoint use = pointService.use(id, amount);
+        return ResponseEntity.ok(use);
     }
 }
